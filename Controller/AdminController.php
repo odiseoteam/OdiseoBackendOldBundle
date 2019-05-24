f14f98f9a20cc353fd943f02ad5e849e8f6e4799
pablo <pablo@pablo.com>
2019-05-24 12:21:20 -0300

    clone of OdiseoBackendBundle

---
+++ Controller/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Odiseo\Bundle\BackendBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as EasyAdminController;
+use Odiseo\Bundle\BackendBundle\Entity\User;
+
+class AdminController extends EasyAdminController
+{
+	
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# OdiseoBackendBundle
+Bundle to create CMS
